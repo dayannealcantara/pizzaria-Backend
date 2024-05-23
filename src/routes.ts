31d74292952b74deb827e7fbb16da4562086def6
dayannealcantara <dayannealcantara14@gmail.com>
2024-05-23 15:55:44 -0300

    feat: add product route

--- src/routes.ts
+++ src/routes.ts
@@ -5,9 +5,14 @@ import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+import { CreateProductController } from './controllers/products/CreateProductController';
+import multer from 'multer';
+import uploadConfig from './config/multer'
 
 
 const router= Router();
+
+const upload = multer(uploadConfig.upload("./tmp"))
 
 //Rotas de usuário
 router.post('/users', new CreateUserController().handle)
@@ -21,5 +26,9 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+//Rotas de produtos
+
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 export { router }
