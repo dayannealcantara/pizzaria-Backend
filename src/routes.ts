e75ec83492da80fd55021221bc2846003521135c
dayannealcantara <dayannealcantara14@gmail.com>
2024-05-24 16:34:14 -0300

    feat: route to send order

--- src/routes.ts
+++ src/routes.ts
@@ -12,6 +12,11 @@ import { ListProductController } from './controllers/products/ListProductControl
 import { CreateOrderController } from './controllers/order/CreateOrderController';
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 import { AddItemController } from './controllers/order/AddItemController';
+import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
+import { ListOrdersController } from './controllers/order/ListOrdersController';
+import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 
 const router= Router();
@@ -43,6 +48,11 @@ router.post('/order', isAuthenticated,  new CreateOrderController().handle)
 router.delete('/order', isAuthenticated,  new RemoveOrderController().handle)
 
 router.post('/order/add', isAuthenticated,  new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated,  new RemoveItemController().handle)
+
+
+router.put('/order/send', isAuthenticated, new SendOrderController().handle )
+
 
 
 
